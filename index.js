ce6e0a6b5bb2b62ea8085b968ba9bdd5bd6a1f5f
Juha Hakala <hakalan.juha@gmail.com>
2021-01-23 21:10:33 +0000

    server running

---
+++ index.js
@@ -0,0 +1,41 @@
+// Dependencies
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
+
+const app = express();
+const redir = express();
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/juhakala.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/juhakala.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/juhakala.com/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+app.use(express.static('./build'));
+
+// redirect http to https
+redir.get('/', (req, res) => {
+	if (!req.secure) {
+//		console.log('redirecting');
+		res.redirect('https://' + req.headers.host + req.url);
+	}
+})
+
+// Starting both http & https servers
+const httpServer = http.createServer(redir);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
