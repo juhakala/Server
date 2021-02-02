ef249dbb2ed8d92c57e55b40a93b3c8c9ddd5d18
Juha Hakala <hakalan.juha@gmail.com>
2021-02-02 05:09:34 +0200

    about and todo, sb-click map, map centering started

--- react-app-site/src/components/Home.js
+++ react-app-site/src/components/Home.js
@@ -1,12 +1,61 @@
+import "./../css/home.css"
+
+const Item = ({ item }) => {
+	var style = item.done === 0 ? 'title': 'notDone';
+	style = item.done === 1 ? 'done' : style;
+	return (
+		<li className={style}>
+			{item.content}
+		</li>
+	)
+}
 const Home = () => {
+	// 0-head, 1-done, 2-not done 
+	const todo = [
+		{done: 0, content: 'Chat'},
+		{done: 1, content: 'connect and disconnect from socket.io server with open and close'},
+		{done: 1, content: 'fetch/show messages from database'},
+		{done: 1, content: 'write messages'},
+		{done: 1, content: 'Scroll down on new message'},
+		{done: 1, content: 'injection/xss protection'},
+		{done: 2, content: 'get author from login'},
+		{done: 2, content: 'report button for messages'},
+		{done: 0, content: 'Map'},
+		{done: 1, content: 'feth from database'},
+		{done: 1, content: 'zoom/move with mouse'},
+		{done: 1, content: 'fullscreen on db-click'},
+		{done: 2, content: 'start on whole map/center'},
+		{done: 2, content: 'show all maps'},
+		{done: 0, content: 'Map-processing'},
+		{done: 2, content: 'update map with data from app'},
+		{done: 2, content: 'make more maps if location outside of current map'},
+		{done: 0, content: 'Android app (kotlin/java)'},
+		{done: 1, content: 'get location data'},
+		{done: 2, content: 'store location data until send to server'},
+		{done: 2, content: 'send location data to server'},
+		{done: 0, content: 'Login'},
+		{done: 2, content: 'nothing yet'},
+		{done: 0, content: 'Admin'},
+		{done: 2, content: 'show reported messages'},
+		{done: 2, content: 'delete/accept reported messages'}
+	]
 	return (
 		<>
-			<h3>[4/5] chat: (socket.io, mysql) minor bugs wth time showing sometimes and with safari</h3>
-			<h1>ATM: Maps</h1>
-			<h2>	[x] zoom and move with mouse</h2>
-			<h2>    [x] tmp picture, top left corner (need to zoom) lines with 10x, 5x, 3x, 1x</h2>
-			<h2>    [ ] get loaction data with app and send to server to process it to the map</h2>
-			<h2>Additional information is printed to console.log atm as needed and tested</h2>
+			<div className='aboutWrap'>
+				<h1>About this site:</h1>
+				<p>
+				<strong>Backend:</strong> Nodejs and Python.<br />
+				<strong>Frontend:</strong> Javascript and React.<br />
+				Own server running on Raspberry Pi 4 for learning.<br />
+				Putting location points to black background thus drawing map of helsinki area.<br />
+				Updating as I learn and have time.</p>
+			</div>
+			<div className='todoWrap'>
+				<h1>To-do:</h1>
+				<ul className='list'>
+					{todo.map((item,i) => <Item key={i} item={item} />)}
+				</ul>
+			</div>
 		</>
 	)
 }
--- react-app-site/src/components/Map.js
+++ react-app-site/src/components/Map.js
@@ -4,9 +4,43 @@ import { MapInteractionCSS } from 'react-map-interaction';
 import './../css/map.css';
 
 const Image = ({map}) => {
+	const [values, setValues] = useState({
+		x: -315, y: -4635, scale: 0.068
+	})
+	const styles = { 
+        transform: `translate(${values.x}px, ${values.y}px) scale(${values.scale})` 
+	};
+/*
+	const EventListenerMode = {capture: true};
+	function mousemoveListener (e) {
+		e.preventDefault();
+		console.log(e.screenX, e.screenY)
+	}
+	const mouseDownEvent = (event) => {
+		console.log(event)
+		document.addEventListener ('mousemove', mousemoveListener, EventListenerMode);
+		document.addEventListener ('mouseup',   mouseupListener,   EventListenerMode);
+		console.log('in');
+	}
+	function mouseupListener (e) {
+		document.removeEventListener ('mouseup',   mouseupListener,   EventListenerMode);
+		document.removeEventListener ('mousemove', mousemoveListener, EventListenerMode);
+		console.log('out');
+	}
+
+	return (
+		<div className='first' onMouseDown={ mouseDownEvent } >
+			<div className='second'>
+				<div style={styles}>
+					<img className='image' src={map} alt='baseImg'/>
+				</div>
+			</div>
+		</div>
+	)
+*/
 	return (
 		<MapInteractionCSS>
-			<img src={map} alt='baseImg'/>
+			<img className='image' src={map} alt='baseImg'/>
 		</MapInteractionCSS>
 	)
 }
@@ -27,12 +61,20 @@ const Map = () => {
 			setMap([`${resp.data}?time=${Date.now()}`]);
 		});
 	}
+	const fullScreen = (event) => {
+		const elem = document.getElementsByClassName('mapWrap')[0];
+		if (elem.requestFullscreen) {
+			elem.requestFullscreen();
+		} else if (elem.webkitRequestFullscreen) { /* Safari */
+			elem.webkitRequestFullscreen();
+		}
+	}
 	return (
 		<div className='mapBody'>
 			<p>map site</p>
-			<div onClick={map_request}>req</div>
-			<div onClick={new_request}>base</div>
-			<div className='mapWrap'>
+			<div className='button' onClick={map_request}>req</div>
+			<div className='button' onClick={new_request}>base</div>
+			<div onDoubleClick={fullScreen} className='mapWrap'>
 				<Image map={map} />
 			</div>
 		</div>
