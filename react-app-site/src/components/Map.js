869eafb593eda2bbd92595f8c325f7de53f26ff6
Juha Hakala <hakalan.juha@gmail.com>
2021-02-08 16:27:24 +0200

    zoom to mouse

--- react-app-site/src/components/Map.js
+++ react-app-site/src/components/Map.js
@@ -5,7 +5,10 @@ import './../css/map.css';
 const Image = ({map}) => {
 	var mouseStart;
 	var imgStart = [0, 0];
-	var imgScale = 1;
+//	var imgScale = 1;
+
+//	var currentMouseY;
+//	var currentMouseX;
 	const EventListenerMode = {capture: true};
 	function mousemoveListener (e) {
 		e.preventDefault();
@@ -26,15 +29,34 @@ const Image = ({map}) => {
 		document.removeEventListener ('mousemove', mousemoveListener, EventListenerMode);
 	}
 	const handleScroll = (event) => {
-		const elem = document.getElementsByClassName('image')[0];
-//		console.log(elem.style.transform);
-		const tmp = imgScale + 0.01 * event.deltaY;
-		if (tmp > 0.5 && tmp < 150) {
-			imgScale = tmp;
-			elem.style.transform = `scale(${imgScale})`;
-			document.getElementById('info').innerHTML = imgScale;
+		const elem1 = document.getElementsByClassName('imgWrap')[0];
+		const bound1 = elem1.getBoundingClientRect();
+
+		const elem2 = document.getElementsByClassName('image')[0];
+		const bound2 = elem2.getBoundingClientRect();
+
+		var factor = 0.98;
+		if (event.deltaY < 0) {
+			factor = 1/factor;
 		}
+		if (parseFloat(bound2.width) * factor > 40000 || parseFloat(bound2.width) * factor < 300 )
+			return ;
+		elem2.style.width = (parseFloat(bound2.width) * factor) + 'px';
+
+		const currentMouseY = event.screenY - bound1.y;
+		const currentMouseX = event.screenX - bound1.x;
+
+		const xx = bound2.x - bound1.x;
+		const yy = bound2.y - bound1.y;
+
+		const dx = (currentMouseX - xx) * (factor - 1);
+		const dy = (currentMouseY - yy) * (factor - 1);
+		elem2.style.left = (xx - dx) + 'px';
+		elem2.style.top = (yy - dy) + 'px';
+//		console.log(dx);
+
 	}
+
 	const disableScroll = () => {
 		document.body.style.overflow = "hidden";
 	}
@@ -51,13 +73,21 @@ const Image = ({map}) => {
 		} else
 			document.exitFullscreen();
 	}
+	const centerToHome = () => {
+		const elem = document.getElementsByClassName('image')[0];
+		elem.style.width = '39999px';
+		elem.style.left = '-19629px';
+		elem.style.top = '-19730px';
+	}
 
 	return (
 		<>
-			<p id='info' className='info'>1</p>
+			<p id='info' className='info'>50</p>
+			<div className="center" onClick={ centerToHome }>center</div>
 			<div onDoubleClick={fullScreen} className='mapWrap'>
 				<div className='imgWrap'
 					onMouseDown={ mouseDownEvent }
+//					onContextMenu={zo}
 					onWheel={ handleScroll }
 					onMouseEnter={ disableScroll }
 					onMouseLeave={ enableScroll }
